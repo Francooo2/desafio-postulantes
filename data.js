47a04d2bbd38fb469782b7c5120f3786b1d8c7e5
Franco Palma <fpalmalopez2@gmail.com>
2022-04-22 13:52:57 -0400

    Se agregan comentarios

--- data.js
+++ data.js
@@ -1,11 +1,14 @@
+// Se importa la biblioteca a utilizar
 const puppeteer = require('puppeteer')
 
 async function init() {
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.sii.cl/servicios_online/1047-nomina_inst_financieras-1714.html');
+    // Se le indica a Puppeteer que trabajaremos con una nueva página
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('https://www.sii.cl/servicios_online/1047-nomina_inst_financieras-1714.html')
 
+    // Se evalua el documento html, devolviendo la información solicitada
     const principalElements = await page.evaluate(() => {
         const title = document.getElementsByClassName('title')[0].textContent.split(')')[1].trim()
         const paragraph = document.getElementsByTagName('p')[1].textContent.replace(/\s+/g, ' ').trim()
@@ -17,6 +20,7 @@ async function init() {
             return cell.textContent
         })
 
+        // Se le da un formato adecuado a la información obtenida
         const elements = {
             title: title,
             paragraph: paragraph,
@@ -25,16 +29,20 @@ async function init() {
             data: data
         }
 
+        // Se retorna el resultado de la evaluación
         return elements
     })
 
 
-    await browser.close();
+    // Se le indica a Puppeteer que finalize el proceso
+    await browser.close()
 
+    // Se retorna el objeto con la información obtenida en un formato adecuado
     return principalElements
 
 }
 
+// Se exporta la función, para ser utilizada en el archivo principal
 module.exports = {
     init
 }
--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+// Se importa Express y la función definida en data.js 
 const express  = require('express')
-const app      = express()
 const { init } = require('./data')
 
+// Se instancia una aplicación de Express
+const app      = express()
+
+// Se define un puerto para la aplicación
 app.set('port', process.env.PORT || 3000)
 
+// Se disponibiliza la ruta raiz del servidor de Express, que ejecuta init() y responde la información en JSON
 app.get('/', async (req, res) => {
     const data = await init()
     res.json(data)
 })
 
+// La aplicación escucha en el puerto asignado
 app.listen(app.get('port'), () => {
     console.log(`Servidor corriendo en el puerto ${app.get('port')}`);
 })
